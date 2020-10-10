5a90ae15f61c52a55079d82b1dc6729e83353224
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-10 03:31:02 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -11,7 +11,8 @@ def fourth_challenge
      },
      :hero=>{
        :name=>"Romeo",
-       :age=>"15"
+       :age=>"15",
+       :status=>"alive"
      },
      :hero_friends=>[
        {
@@ -38,7 +39,8 @@ def fourth_challenge
  },
  :heroine=>{
    :name=>"Juliet",
-   :age=>"15"
+   :age=>"15",
+   :status=>"alive"
  },
  :heroine_friends=>[
    {
