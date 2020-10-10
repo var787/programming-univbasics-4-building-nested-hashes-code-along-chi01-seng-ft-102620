030f6f9941aa673f1d2b993f4c34df92f13ea8d1
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-10 03:30:27 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
      },
      :hero=>{
        :name=>"Romeo",
-       :age=>"15"
+       :age=>"15",
+       :status=>"alive"
      },
      :hero_friends=>[]
  },
@@ -26,7 +27,8 @@ def third_challenge
  },
  :heroine=>{
    :name=>"Juliet",
-   :age=>"15"
+   :age=>"15",
+   :status=>"alive"
  },
  :heroine_friends=>[]
 }
